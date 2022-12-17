07e9dd8e0c7d3173df2d809966b74efe628b6caa
sw08 <lswja6866@naver.com>
2022-12-18 01:23:45 +0900

    [FEAT] Add integral function

--- Cogs/02Math.py
+++ Cogs/02Math.py
@@ -64,7 +64,60 @@ class Math(commands.Cog):
             await ctx.respond(embed=embed)
         except Exception as e:
             raise errors.WrongExpression
-
+    
+    @commands.slash_command(name="적분", description="미지수가 1개인 f(x)를 적분")
+    @commands.max_concurrency(3, commands.BucketType.default)
+    @commands.cooldown(1, 30, commands.BucketType.user)
+    async def integral(
+        self,
+        ctx,
+        expression: str,
+        section: Option(str, "정적분 구간을 /로 구분해 입력", name="구간", default=None),
+    ):
+        # try:
+        if section is not None:
+            splitted = section.split('/')
+            a = int(splitted[0])
+            b = int(splitted[1])
+        f_x = expression
+        expression = utils.math.Expression(expression)
+        if expression.terms[0].degree > 10:
+            if section is not None:
+                embed = discord.Embed(
+                    title="⚠️ 명령어 사용 제한",
+                    description="10차 방정식 이상은 정적분 계산이 제한되었습니다",
+                    color=warncolor,
+                )
+                await ctx.respond(embed=embed)
+                return
+        if len(expression.terms) > 20:
+            embed = discord.Embed(
+                title="⚠️ 명령어 사용 제한",
+                description="항은 20개 이상 입력 불가합니다",
+                color=warncolor,
+            )
+            await ctx.respond(embed=embed)
+            return
+        F_x = []
+        for i in expression.terms:
+            temp = i
+            temp.degree += 1
+            temp.coefficient /= temp.degree
+            F_x.append(temp)
+        F_x = utils.math.Expression.from_terms(F_x)
+        embed = discord.Embed(title="적분 결과", color=embedcolor)
+        embed.add_field(name="f(x)", value=f_x, inline=False)
+        embed.add_field(
+            name="F(x)",
+            value=str(F_x) + ' + C',
+            inline=False,
+        )
+        if section is not None:
+            result = F_x.substitute(b) - F_x.substitute(a) if a != b else 0
+            embed.add_field(name=f"F({b}) - F({a})", value=str(result), inline=False)
+        await ctx.respond(embed=embed)
+        # except Exception as e:
+        #     raise errors.WrongExpression
 
 def setup(bot):
     bot.add_cog(Math(bot))
